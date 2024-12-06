9a40af6dcc3c722703a3aaeb3a93e607ddffe8d3
XuananLe <lexuanan18102004@gmail.com>
2024-12-06 17:55:12 +0700

    Update

--- data_generators/main.py
+++ data_generators/main.py
@@ -1,21 +1,19 @@
 import time
-import threading
 import numpy as np
-from flask import Flask, jsonify, Response
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 
 def generate_data():
-    x = 0
     while True:
+        x = np.random.uniform(0, 10)
         noise = np.random.normal(0, 1)
         y = 2 * x + noise
         timestamp = time.time()
 
         yield f"data: {{'x': {x}, 'y': {y}, 'timestamp': {timestamp}}}\n\n"
         
-        x += 1
         time.sleep(1)
 
 @app.route('/data')
